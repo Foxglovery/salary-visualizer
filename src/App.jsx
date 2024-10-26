1339eb33fcf21aa571dc9535cfbcd77819c12e6c
Foxglovery <foxglovery@gmail.com>
2024-10-26 14:05:47 -0500

    added css dollar bill

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import dollarBill from './assets/10-dollar.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       
       <h1>Salary Visualizer</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-       
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={dollarBill} alt='Dollar Bill' className='dollar-bill'/>
+      
+      
     </>
   )
 }
